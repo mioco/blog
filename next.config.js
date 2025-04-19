da75d09228937fe0869f9429d0fd62e1a91742a3
osyo <mioco1997@gmail.com>
2025-04-19 16:29:09 +0000

    cp CNAME

--- next.config.js
+++ next.config.js
@@ -4,18 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // 在构建完成后复制 CNAME 文件
-  async afterBuild({ outDir }) {
-    const fs = require('fs');
-    const path = require('path');
-    const cnamePath = path.join(process.cwd(), 'CNAME');
-    const outCnamePath = path.join(outDir, 'CNAME');
-    
-    if (fs.existsSync(cnamePath)) {
-      fs.copyFileSync(cnamePath, outCnamePath);
-      console.log('CNAME file copied to build output');
-    }
-  },
 };
 
 module.exports = nextConfig; 
